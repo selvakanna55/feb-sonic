27a85c0913d80d26181e0f65cfcdb561c5331696
selvakanna55 <selvakanna2409@gmail.com>
2022-04-26 09:46:02 +0530

    Update LinkedListExample.java

--- src/LinkedList/LinkedListExample.java
+++ src/LinkedList/LinkedListExample.java
@@ -1,52 +1,77 @@
 package LinkedList;
 
-import java.util.Collections;
-
 class Node{
     int value;
-    Node next; // custom datatype
-    Node(){}
+    Node  next;
     Node(int value){
         this.value = value;
     }
 }
 
-public class LinkedListExample {
+public class LinkedListExample{
     Node head = null;
-    void createNewLL(int val){
+    int length = 0;
+
+
+    void insertAtPos(int pos, int val){
+        if(pos<1) return;
         Node newNode = new Node(val);
-        head = newNode;
+        if(pos==1){
+            newNode.next = head;
+            head = newNode;
+        }else{
+            Node curr = head;
+            while (pos-2!=0){
+                curr = curr.next;
+                pos--;
+            }
+            Node nextNode = curr.next;
+            newNode.next = nextNode;
+            curr.next = newNode;
+        }
+        length++;
     }
 
+    void deleteAtPos(int pos){
+        if(pos<1) return;
+        if(pos==1) head = head.next;
+        else{
+            Node curr = head;
+            while (pos-2!=0){
+                curr = curr.next;
+                pos--;
+            }
+            Node temp = curr.next;
+            curr.next = curr.next.next;
+            temp.next = null;
+        }
+        length--;
+    }
+
+
     void printLL(){
-        Node temp = head;
-        while(temp!=null){
-            System.out.print(temp.value+" ");
-            temp=  temp.next;
+        Node curr = head;
+        while(curr!=null){
+            System.out.print(curr.value+" ");
+            curr = curr.next;
         }
         System.out.println();
     }
-
-    void insertValueAtEnd(int val){
-        Node temp = head;
-        Node newNode = new Node(val);
-        while(temp.next!=null){
-            temp = temp.next;
-        }
-        temp.next = newNode;
-    }
     public static void main(String[] args) {
         LinkedListExample ll = new LinkedListExample();
-        ll.createNewLL(100);
-//        ll.head.next = new Node(101);;
-        //        ll.head.next.next = new Node(102);
+        ll.insertAtPos(1, 10);
+        ll.insertAtPos(2, 20);
         ll.printLL();
-        ll.insertValueAtEnd(101);
+        ll.insertAtPos(3, 30);
         ll.printLL();
-        ll.insertValueAtEnd(102);
+        ll.insertAtPos(4, 40);
+        ll.insertAtPos(1, 999);
         ll.printLL();
-        ll.insertValueAtEnd(199);
+        ll.deleteAtPos(2);
+        ll.printLL();
+        ll.deleteAtPos(1);
+        ll.printLL();
+        ll.deleteAtPos(ll.length);
         ll.printLL();
     }
 }
-
